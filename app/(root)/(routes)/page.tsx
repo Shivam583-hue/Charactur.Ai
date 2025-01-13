f6f4184a811c96776339a70487611e79d4810186
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-13 19:32:13 +0600

    Preview card for characters created

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,39 @@
 import Categories from '@/components/categories';
+import Companions from '@/components/Companions';
 import SearchInput from '@/components/SearchInput'
 import prismadb from '@/lib/prismadb';
 import React, { Suspense } from 'react';
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+
+  const categoryId = await searchParams.categoryId
+  const name = await searchParams.name
+
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: categoryId,
+      name: {
+        search: name,
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }
+  });
 
   const categories = await prismadb.category.findMany()
 
@@ -12,6 +42,7 @@ const RootPage = async () => {
       <Suspense fallback={<div>Loading Search...</div>}>
         <SearchInput />
         <Categories data={categories} />
+        <Companions data={data} />
       </Suspense>
     </div>
   )
