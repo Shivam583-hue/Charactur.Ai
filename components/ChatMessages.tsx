25818ec0649571fabe88386e28066f207005bc23
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-14 14:00:26 +0600

    added class with all the functions  for all the redis and pinecone functions

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -11,10 +11,30 @@ interface ChatMessagesProps {
 }
 
 const ChatMessages = ({ messages, isLoading, companion }: ChatMessagesProps) => {
+
+  const [fakeLoading, setFakeLoading] = React.useState(messages.length === 0 ? true : false)
+  const scrollRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [messages.length])
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setFakeLoading(false)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className='flex-1 overflow-y-auto pr-4'>
-      <ChatMessage src={companion.src} role="system" content={`Hello, I am ${companion.name}, ${companion.description}`} />
-      <ChatMessage src={companion.src} role="user" content={`Hello, I am ${companion.name}, ${companion.description}`} />
+      <ChatMessage isLoading={fakeLoading} src={companion.src} role="system" content={`Hello, I am ${companion.name}, ${companion.description}`} />
+      {messages.map((message) => (
+        <ChatMessage key={message.content} isLoading={isLoading} src={companion.src} role={message.role} content={message.content} />
+      ))}
+      {isLoading && <ChatMessage isLoading src={companion.src} role="system" />}
+      <div ref={scrollRef} />
     </div>
   )
 }
