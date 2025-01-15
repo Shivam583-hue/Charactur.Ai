c3506365fb5c752c9f393ea2b6a7ecaee50ab5e9
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-15 19:05:35 +0600

    ui changes final

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center px-6 py-12 text-center bg-white'>
+    <div className='flex min-h-screen flex-col items-center justify-center px-6 py-12 text-center bg-white min-w-screen'>
       <SignUp />
     </div>
   )
