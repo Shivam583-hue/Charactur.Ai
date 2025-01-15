0b08395ca0bb0ddba604d9f1de1f367db0fc4d1c
Shivam583-hue <shivamshivamshivam456@gmail.com>
2025-01-15 19:10:49 +0600

    last commit for the day

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center px-6 py-12 text-center bg-white min-w-screen'>
+    <div className='flex min-h-screen flex-col items-center justify-center px-6 py-12 text-center bg-white w-screen'>
       <SignIn />
     </div>
   )
